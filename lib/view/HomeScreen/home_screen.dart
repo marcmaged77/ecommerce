e45234b5228154507b524be6cf2165654889962a
marcmaged77 <marcmgedbaky@hotmail.com>
2021-06-08 07:20:02 +0200

    to homeview

--- lib/view/HomeScreen/home_screen.dart
+++ lib/view/HomeScreen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:souq/components/customTextField.dart';
+import 'package:souq/constants.dart';
 
 class homeScreen extends StatelessWidget {
   final List<String> names = <String>[
@@ -12,6 +13,11 @@ class homeScreen extends StatelessWidget {
     's',
     "s",
   ];
+
+
+
+
+
 
   late String email;
   late String? name;
@@ -33,48 +39,71 @@ class homeScreen extends StatelessWidget {
     var height = size.height;
     var width = size.width;
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: size.height * 0.060, left: 20, right: 10),
-        child: Column(
-          children: <Widget>[
+        appBar: AppBar(
+          elevation: 0,
+          toolbarHeight: 88, // Set this height
+          flexibleSpace:
+
+          Container(
+            padding: EdgeInsets.only(top: height * 0.02),
+            color: Colors.grey.shade50,
+            child: Column(children: <Widget>[
+              SizedBox(height: 8),
+              Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child:  textFormField()),
+            ]),
+          ),
+
+
+        ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only( left: 20, right: 10),
+          child: Column(
+            children: <Widget>[
 
 
 
 
-            //textfield
-            textFormField(),
+              //textfield
 
 
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "Categories",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                )),
-            SizedBox(
-              height: 30,
-            ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Categories",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  )),
+              SizedBox(
+                height: 30,
+              ),
 
 
-            //listview
-            ListViewCategories(names: names),
-            
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-              Text('Best Selling', style: TextStyle( fontSize: 20, fontWeight: FontWeight.w600)),
-                   FlatButton(
-                       onPressed: (){print('see all');},
-                       child: Text('See All', style: TextStyle( fontSize: 17))),
+              //listview
+              ListViewCategories(names: names, ),
 
 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                Text('Best Selling', style: TextStyle( fontSize: 20, fontWeight: FontWeight.w600)),
+                     FlatButton(
+                         onPressed: (){print('see all');},
+                         child: Text('See All', style: TextStyle( fontSize: 17))),
 
-            ],)
-          ],
+
+
+              ],),
+
+
+              ListViewProducts(names: names),
+              SizedBox(height: 1000,)
+            ],
+          ),
         ),
       ),
     );
@@ -91,10 +120,16 @@ class ListViewProducts extends StatelessWidget {
 
   final List<String> names;
 
+
   @override
   Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
+    var height = size.height;
+    var width = size.width;
     return Container(
-      height: 100,
+      height: 350,
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
         itemCount: names.length,
@@ -104,27 +139,47 @@ class ListViewProducts extends StatelessWidget {
               decoration: BoxDecoration(
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.grey.withOpacity(0.3),
+                      color: Colors.grey.withOpacity(0.1),
                       //color of shadow
                       spreadRadius: 1,
                       //spread radius
                       offset:
-                      Offset(0, 01), // changes position of shadow
+                      Offset(0, 001), // changes position of shadow
                       //first paramerter of offset is left-right
                       //second parameter is top to down
                     ),
                     //you can set more BoxShadow() here
                   ],
-                  borderRadius: BorderRadius.circular(55),
                   color: Colors.white),
-              height: 60,
-              width: 60,
-              child: Image.asset('assets/homeview/Icon_Mens Shoe.png'),
+              height: height * .4,
+              width: width * .4,
+              child: Column(
+                children:<Widget> [
+                  Container(
+                   child:   Image.asset('assets/homeview/product2.png')
+                  ),
+                  SizedBox(height: 10,),
+                  Container(
+                    padding: EdgeInsets.only(left: 5),
+                      alignment: Alignment.topLeft,
+                      child: Text("Leather Wrist Watch",style: TextStyle(fontSize: 17),)),
+
+                  Container(
+                      padding: EdgeInsets.only(left: 5),
+                      alignment: Alignment.topLeft,
+                      child: Text("Leather Wrist Watch",style: TextStyle(fontSize: 13, color: Colors.grey),)),
+
+                  Container(
+                      padding: EdgeInsets.only(left: 5, top: 10),
+                      alignment: Alignment.topLeft,
+                      child: Text("700 Egp",style: TextStyle(fontSize: 17, color: kPrimaryColor),)),
+
+
+                ],
+
+              )
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(names[index])
+
           ]);
         },
         separatorBuilder: (context, index) => SizedBox(
@@ -134,13 +189,33 @@ class ListViewProducts extends StatelessWidget {
     );
   }
 }
+
+
 class ListViewCategories extends StatelessWidget {
-  const ListViewCategories({
+   ListViewCategories({
     Key? key,
+
     required this.names,
   }) : super(key: key);
 
   final List<String> names;
+
+  final List <String> imgs = <String> [
+    'assets/homeview/Icon_Mens Shoe.png',
+    'assets/homeview/women.png',
+    'assets/homeview/Devices.png',
+    'assets/homeview/Icon_Gadgets.png',
+    'assets/homeview/Icon_Gaming.png',
+    'assets/homeview/Icon_Gaming.png',
+    'assets/homeview/Icon_Gaming.png',
+    'assets/homeview/Icon_Gaming.png',
+
+
+
+  ];
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -170,7 +245,7 @@ class ListViewCategories extends StatelessWidget {
                   color: Colors.white),
               height: 60,
               width: 60,
-              child: Image.asset('assets/homeview/Icon_Mens Shoe.png'),
+              child: Image.asset(imgs[index]),
             ),
             SizedBox(
               height: 10,
