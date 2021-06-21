5093e01e01c9a68cd9deb2b4df42864bfd0c75fb
marcmaged77 <marcmgedbaky@hotmail.com>
2021-06-21 13:34:32 +0200

    6/21/2021

---
+++ lib/view/widgets/alertLocation/alert_location.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+
+class LocationAlert extends StatelessWidget {
+  const LocationAlert({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
+    var height = size.height;
+    var width = size.width;
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+
+
+
+          ],
+        ),
+
+
+      ),
+    );
+  }
+}
